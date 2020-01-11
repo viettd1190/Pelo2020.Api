c847c5c2228e970da6b23fe01fa8edaf44245af5
Việt Trần <viettd1190@gmail.com>
2020-01-11 12:25:35 +0700

    Add api get all product

---
+++ src/Pelo.Api/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pelo.Api.Services.MasterServices;
+using Pelo.Api.Services.UserServices;
+using Pelo.Common.Dtos.Product;
+
+namespace Pelo.Api.Controllers
+{
+    [ApiVersion("1.0")]
+    [Produces("application/json")]
+    [ApiController]
+    public class ProductController : BaseController
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IAccountService accountService,
+                                 IProductService productService) : base(accountService)
+        {
+            _productService = productService;
+        }
+
+        /// <summary>
+        ///     Lấy tất cả sản phẩm
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/product/all")]
+        public async Task<ActionResult<IEnumerable<ProductSimpleModel>>> GetAll()
+        {
+            return Ok(await _productService.GetAll(await GetUserId()));
+        }
+    }
+}
