6e106bf95415ab674a9f911c22702d12b3ff353d
tan tran <tantranvan9463@gmail.com>
2020-02-12 00:21:01 +0700

    update manufacturer

---
+++ src/Pelo.Api/Controllers/ManufacturerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pelo.Api.Services.InvoiceServices;
+using Pelo.Api.Services.UserServices;
+using Pelo.Common.Dtos.Manufacturer;
+using Pelo.Common.Models;
+
+namespace Pelo.Api.Controllers
+{
+    [ApiVersion("1.0")]
+    [Produces("application/json")]
+    [ApiController]
+    public class ManufacturerControlle : BaseController
+    {
+        private readonly IManufacturerService _ManufacturerService;
+
+        public ManufacturerControlle(IAccountService accountService,
+                                   IManufacturerService ManufacturerService) : base(accountService)
+        {
+            _ManufacturerService = ManufacturerService;
+        }
+
+        /// <summary>
+        ///     Lấy tất cả hình thức thanh toán
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/manufacturer/all")]
+        public async Task<ActionResult<IEnumerable<ManufacturerSimpleModel>>> GetAll()
+        {
+            return Ok(await _ManufacturerService.GetAll(await GetUserId()));
+        }
+
+        /// <summary>
+        ///     Lấy tất cả hình thức thanh toán
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/manufacturer")]
+        public async Task<ActionResult<PageResult<GetManufacturerPagingResponse>>> GetPaging([FromQuery] GetManufacturerPagingRequest request)
+        {
+            return Ok(await _ManufacturerService.GetPaging(await GetUserId(), request));
+        }
+        /// <summary>
+        ///     Lấy hình thức thanh toán
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/manufacturer/{id}")]
+        public async Task<ActionResult<ManufacturerModel>> GetById(int id)
+        {
+            return Ok(await _ManufacturerService.GetById(await GetUserId(), id));
+        }
+        /// <summary>
+        ///     insert hình thức thanh toán
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("api/manufacturer")]
+        public async Task<ActionResult<bool>> Insert([FromBody] InsertManufacturer request)
+        {
+            return Ok(await _ManufacturerService.Insert(await GetUserId(), request));
+        }
+
+        /// <summary>
+        ///     update hình thức thanh toán
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("api/manufacturer")]
+        public async Task<ActionResult<bool>> Update([FromBody] UpdateManufacturer request)
+        {
+            return Ok(await _ManufacturerService.Update(await GetUserId(), request));
+        }
+        /// <summary>
+        ///     delete hình thức thanh toán
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("api/manufacturer/{id}")]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            return Ok(await _ManufacturerService.Delete(await GetUserId(), id));
+        }
+    }
+}
--- src/Pelo.Api/Services/SqlQuery.cs
+++ src/Pelo.Api/Services/SqlQuery.cs
@@ -167,7 +167,55 @@
                                                              AND c.IsDeleted = 0;";
 
         #endregion
+        #region Manufacturer
 
+        public const string MANUFACTURER_GET_ALL = @"SELECT Id,
+                                                          Name
+                                                   FROM dbo.Manufacturer
+                                                   WHERE IsDeleted = 0;";
+
+        public const string MANUFACTURER_GET_BY_ID = @"SELECT * FROM dbo.Manufacturer WHERE Id = @Id AND IsDeleted = 0";
+
+        public const string MANUFACTURER_INSERT = @"INSERT dbo.Manufacturer
+                                                        (Name,
+                                                         UserCreated,
+                                                         DateCreated,
+                                                         UserUpdated,
+                                                         DateUpdated,
+                                                         IsDeleted)
+                                                 VALUES (@Name,
+                                                         @UserCreated,
+                                                         @DateCreated,
+                                                         @UserUpdated,
+                                                         @DateUpdated,
+                                                         0);
+                                                 SELECT CAST(SCOPE_IDENTITY() as int);";
+
+        public const string MANUFACTURER_UPDATE = @"  UPDATE dbo.Manufacturer
+                                                  SET Name = @Name,
+                                                      UserUpdated = @UserUpdated,
+                                                      DateUpdated = @DateUpdated
+                                                  WHERE Id = @Id";
+
+        public const string MANUFACTURER_DELETE = @"  UPDATE dbo.Manufacturer
+                                                  SET UserUpdated = @UserUpdated,
+                                                      DateUpdated = @DateUpdated,
+                                                      IsDeleted = 1
+                                                  WHERE Id = @Id";
+
+        public const string MANUFACTURER_PAGING = @"SELECT c.Id,
+                                                              c.Name,
+                                                              c.DateUpdated
+                                                       FROM dbo.Manufacturer c
+                                                       WHERE ISNULL(c.Name, '') COLLATE Latin1_General_CI_AI LIKE @Name COLLATE Latin1_General_CI_AI
+                                                             AND c.IsDeleted = 0
+                                                       ORDER BY {0} {1} OFFSET @Skip ROWS FETCH NEXT @Take ROWS ONLY;
+                                                       SELECT COUNT(*)
+                                                       FROM dbo.Manufacturer c
+                                                       WHERE ISNULL(c.Name, '') COLLATE Latin1_General_CI_AI LIKE @Name COLLATE Latin1_General_CI_AI
+                                                             AND c.IsDeleted = 0;";
+
+        #endregion
         #region Province
 
         public const string PROVINCE_GET_ALL = @"SELECT Id,
