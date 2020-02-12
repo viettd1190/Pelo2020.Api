7866591e2ae33ef09a8e2f93242150351a6220e4
tan tran <tantranvan9463@gmail.com>
2020-02-12 22:01:39 +0700

    update candidate status

---
+++ src/Pelo.Api/Services/CandidateServices/CrmStatusService.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Pelo.Api.Services.BaseServices;
+using Pelo.Api.Services.UserServices;
+using Pelo.Common.Dtos.CandidateStatus;
+using Pelo.Common.Models;
+using Pelo.Common.Repositories;
+
+namespace Pelo.Api.Services.CandidateServices
+{
+    public interface ICandidateStatusService
+    {
+        Task<TResponse<IEnumerable<CandidateStatusSimpleModel>>> GetAll(int userId);
+        Task<TResponse<PageResult<GetCandidateStatusPagingResponse>>> GetPaging(int v, GetCandidateStatusPagingRequest request);
+
+        Task<TResponse<GetCandidateStatusResponse>> GetById(int userId, int id);
+
+        Task<TResponse<bool>> Insert(int userId, InsertCandidateStatus request);
+
+        Task<TResponse<bool>> Update(int userId, UpdateCandidateStatus request);
+
+        Task<TResponse<bool>> Delete(int userId, int id);
+    }
+
+    public class CandidateStatusService : BaseService,
+                                    ICandidateStatusService
+    {
+        readonly IRoleService _roleService;
+
+        public CandidateStatusService(IDapperReadOnlyRepository readOnlyRepository,
+                                IDapperWriteRepository writeRepository,
+                                IHttpContextAccessor context,
+                                IRoleService roleService) : base(readOnlyRepository,
+                                                                 writeRepository,
+                                                                 context)
+        {
+            _roleService = roleService;
+        }
+
+        public async Task<TResponse<bool>> Delete(int userId, int id)
+        {
+            try
+            {
+                var canGetAll = await CanGetAll(userId);
+                if (canGetAll.IsSuccess)
+                {
+                    var data = await GetById(userId, id);
+                    if (data.IsSuccess)
+                    {
+                        var result = await WriteRepository.ExecuteScalarAsync<int>(SqlQuery.CANDIDATE_STATUS_DELETE,
+                                                                                                              new
+                                                                                                              {
+                                                                                                                  Id = id,
+                                                                                                                  UserUpdated = userId,
+                                                                                                                  DateUpdated = DateTime.Now
+                                                                                                              });
+                        if (result.IsSuccess)
+                        {
+                            return await Ok(true);
+                        }
+                        return await Fail<bool>(result.Message);
+                    }
+                    return await Fail<bool>(data.Message);
+                }
+                return await Fail<bool>(canGetAll.Message);
+            }
+            catch (Exception exception)
+            {
+                return await Fail<bool>(exception);
+            }
+        }
+
+        #region ICandidateStatusService Members
+
+        public async Task<TResponse<IEnumerable<CandidateStatusSimpleModel>>> GetAll(int userId)
+        {
+            try
+            {
+                var canGetAll = await CanGetAll(userId);
+                if (canGetAll.IsSuccess)
+                {
+                    var result = await ReadOnlyRepository.QueryAsync<CandidateStatusSimpleModel>(SqlQuery.CANDIDATE_STATUS_GET_ALL);
+                    if (result.IsSuccess)
+                    {
+                        return await Ok(result.Data);
+                    }
+
+                    return await Fail<IEnumerable<CandidateStatusSimpleModel>>(result.Message);
+                }
+
+                return await Fail<IEnumerable<CandidateStatusSimpleModel>>(canGetAll.Message);
+            }
+            catch (Exception exception)
+            {
+                return await Fail<IEnumerable<CandidateStatusSimpleModel>>(exception);
+            }
+        }
+
+        public async Task<TResponse<GetCandidateStatusResponse>> GetById(int userId, int id)
+        {
+            try
+            {
+                var canGetAll = await CanGetAll(userId);
+                if (canGetAll.IsSuccess)
+                {
+                    var result = await ReadOnlyRepository.QueryFirstOrDefaultAsync<GetCandidateStatusResponse>(SqlQuery.CANDIDATE_STATUS_GET_BY_ID,
+                                                                                                              new
+                                                                                                              {
+                                                                                                                  Id = id,
+                                                                                                              });
+                    if (result.IsSuccess)
+                    {
+                        return await Ok(result.Data);
+                    }
+
+                    return await Fail<GetCandidateStatusResponse>(result.Message);
+                }
+
+                return await Fail<GetCandidateStatusResponse>(canGetAll.Message);
+            }
+            catch (Exception exception)
+            {
+                return await Fail<GetCandidateStatusResponse>(exception);
+            }
+        }
+
+        public async Task<TResponse<PageResult<GetCandidateStatusPagingResponse>>> GetPaging(int userId, GetCandidateStatusPagingRequest request)
+        {
+            try
+            {
+                var canGetAll = await CanGetAll(userId);
+                if (canGetAll.IsSuccess)
+                {
+                    var result = await ReadOnlyRepository.QueryMultipleLFAsync<GetCandidateStatusPagingResponse, int>(string.Format(SqlQuery.CANDIDATE_STATUS_GET_BY_PAGING,
+                                                                                                                         request.ColumnOrder,
+                                                                                                                         request.SortDir.ToUpper()),
+                                                                                                              new
+                                                                                                              {
+                                                                                                                  Name = $"%{request.Name}%",
+                                                                                                                  Skip = (request.Page - 1) * request.PageSize,
+                                                                                                                  Take = request.PageSize
+                                                                                                              });
+                    if (result.IsSuccess)
+                    {
+                        return await Ok(new PageResult<GetCandidateStatusPagingResponse>(request.Page,
+                                                                                  request.PageSize,
+                                                                                  result.Data.Item2,
+                                                                                  result.Data.Item1));
+                    }
+
+                    return await Fail<PageResult<GetCandidateStatusPagingResponse>>(result.Message);
+                }
+
+                return await Fail<PageResult<GetCandidateStatusPagingResponse>>(canGetAll.Message);
+            }
+            catch (Exception exception)
+            {
+                return await Fail<PageResult<GetCandidateStatusPagingResponse>>(exception);
+            }
+        }
+
+        public async Task<TResponse<bool>> Insert(int userId, InsertCandidateStatus request)
+        {
+            try
+            {
+                var canGetAll = await CanGetAll(userId);
+                if (canGetAll.IsSuccess)
+                {
+                    var result = await WriteRepository.ExecuteScalarAsync<int>(SqlQuery.CANDIDATE_STATUS_INSERT,
+                                                                                                              new
+                                                                                                              {
+                                                                                                                  request.Name,
+                                                                                                                  request.Color,
+                                                                                                                  request.SortOrder,
+                                                                                                                  UserCreated = userId,
+                                                                                                                  DateCreated = DateTime.Now,
+                                                                                                                  UserUpdated = userId,
+                                                                                                                  DateUpdated = DateTime.Now
+                                                                                                              });
+                    if (result.IsSuccess)
+                    {
+                        return await Ok(true);
+                    }
+                    return await Fail<bool>(result.Message);
+                }
+                return await Fail<bool>(canGetAll.Message);
+            }
+            catch (Exception exception)
+            {
+                return await Fail<bool>(exception);
+            }
+        }
+
+        public async Task<TResponse<bool>> Update(int userId, UpdateCandidateStatus request)
+        {
+            try
+            {
+                var canGetAll = await CanGetAll(userId);
+                if (canGetAll.IsSuccess)
+                {
+                    var data = await GetById(userId, request.Id);
+                    if (data.IsSuccess)
+                    {
+                        var result = await WriteRepository.ExecuteScalarAsync<int>(SqlQuery.CANDIDATE_STATUS_UPDATE,
+                                                                                                              new
+                                                                                                              {
+                                                                                                                  request.Id,
+                                                                                                                  request.Name,
+                                                                                                                  request.Color,
+                                                                                                                  request.SortOrder,
+                                                                                                                  UserUpdated = userId,
+                                                                                                                  DateUpdated = DateTime.Now
+                                                                                                              });
+                        if (result.IsSuccess)
+                        {
+                            return await Ok(true);
+                        }
+                        return await Fail<bool>(result.Message);
+                    }
+                    return await Fail<bool>(data.Message);
+                }
+                return await Fail<bool>(canGetAll.Message);
+            }
+            catch (Exception exception)
+            {
+                return await Fail<bool>(exception);
+            }
+        }
+
+        #endregion
+
+        private async Task<TResponse<bool>> CanGetAll(int userId)
+        {
+            try
+            {
+                var checkPermission = await _roleService.CheckPermission(userId);
+                if (checkPermission.IsSuccess)
+                {
+                    return await Ok(true);
+                }
+
+                return await Fail<bool>(checkPermission.Message);
+            }
+            catch (Exception exception)
+            {
+                return await Fail<bool>(exception);
+            }
+        }
+    }
+}
